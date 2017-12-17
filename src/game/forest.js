334c3605cfeb8cdfd9b879ce7aa167cc757976f6
Teemu Kuikka <teemu.kuikka@student.tut.fi>
2017-12-17 16:50:25 +0200

    Add third tree and rock, refactor forest generator

--- src/game/forest.js
+++ src/game/forest.js
@@ -7,18 +7,30 @@ export default class Forest {
 
 		this.stage = stage;
 		this.mapdata = mapdata;
-		this.randomTrees = ['/static/tree1.svg', '/static/tree2.svg'];
-		this.scales = [0.9,1.0,1.1];
+		this.randomTrees = [
+			'/static/tree1.svg',
+			'/static/tree1.svg', // Make tree1 twice as likely
+			'/static/tree2.svg',
+			'/static/tree3.svg'
+		];
+		this.scales = [0.09, 0.095, 0.1, 0.105, 0.11];
 		this.obstacles = [];
 		this.distanceToRoad = 60;
 		this.distanceToLog = 60;
 		this.distanceToLogDeposit = 100;
+		this.padding = 2000;
+		this.rockFrequency = 0.5;
+
+		this.xMin = this.yMin = this.xMax = this.yMax = 0;
 		
 		for (var i = 0; i < this.mapdata.logdeposits.length; i++) {
 			this.obstacles.push({type:'logdeposit', x:this.mapdata.logdeposits[i].x, y:this.mapdata.logdeposits[i].y});
+			this.updateMinMax(this.mapdata.logdeposits[i]);
+
 		}
 		for (var i = 0; i < this.mapdata.logs.length; i++) {
 			this.obstacles.push({type:'log', x:this.mapdata.logs[i].x, y:this.mapdata.logs[i].y});
+			this.updateMinMax(this.mapdata.logs[i]);
 		}
 		for (var i = 0; i < this.mapdata.routes.length; i++) {
 			for (var j = 0; j < this.mapdata.routes[i].to.length; j++) {
@@ -26,47 +38,77 @@ export default class Forest {
 				var end = {x:this.mapdata.routes[this.mapdata.routes[i].to[j]-1].x,y:this.mapdata.routes[this.mapdata.routes[i].to[j]-1].y};
 				this.obstacles.push({type:'route', startpoint:start, endpoint:end});
 			}
+			this.updateMinMax(this.mapdata.routes[i]);
 		}
 	}
 
 	buildTrees() {
 		var inrange = false;
-		for (var y = -50; y < 50; y++) {
-			for (var x = -50; x < 50; x++) {
+		var inrange_rock = false;
+		var y = this.yMin - this.padding;
+		var x = this.xMin - this.padding;
+		while (y <= (this.yMax + this.padding)) {
+			x = this.xMin - this.padding;
+			while (x <= (this.xMax + this.padding)) {
 				inrange = false;
+				inrange_rock = false;
 				//Check that no logs, roads, logdeposits, etc in the way
 				//Random offset per tree in square
     			//if square is from 0 to 100, get offset from 30 to 70
-    			var x_offset = x*100 + (Math.floor(Math.random() * (70 - 30 + 1)) - 30);
-    			var y_offset = y*100 + (Math.floor(Math.random() * (70 - 30 + 1)) - 30);
-				for (var i = 0; i < this.obstacles.length; i++) {
-					if (this.obstacles[i].type === 'route' && distanceToSegment({x:x_offset, y:y_offset}, this.obstacles[i].startpoint, this.obstacles[i].endpoint) < this.distanceToRoad) {
-						inrange = true;
-						break;
-					}
-					else if (this.obstacles[i].type === 'log' && distance({x:x_offset, y:y_offset}, {x:this.obstacles[i].x, y:this.obstacles[i].y}) < this.distanceToLog) {
-						inrange = true;
-						break;
-					}
-					else if (this.obstacles[i].type === 'logdeposit' && distance({x:x_offset, y:y_offset}, {x:this.obstacles[i].x, y:this.obstacles[i].y}) < this.distanceToLogDeposit) {
-						inrange = true;
-						break;
-					}
+    			var x_offset = x + (Math.floor(Math.random() * (70 - 30 + 1)) + 30);
+				var y_offset = y + (Math.floor(Math.random() * (70 - 30 + 1)) + 30);
+				var x_offset_rock = x + (Math.floor(Math.random() * (90 - 10 + 1)) + 10);
+				var y_offset_rock = y + (Math.floor(Math.random() * (90 - 10 + 1)) + 10);
+				
+				inrange = this.checkRange(this.obstacles, x_offset, y_offset);
+				inrange_rock = this.checkRange(this.obstacles, x_offset_rock, y_offset_rock);
+
+				if (!inrange_rock && Math.random() < this.rockFrequency) {
+					this.drawSprite('/static/rock.svg', x_offset_rock, y_offset_rock);					
 				}
 
-				if (inrange) continue;
+				if (!inrange) {
+					this.drawSprite(this.randomTrees[Math.floor(Math.random()*this.randomTrees.length)], x_offset, y_offset);
+				}
 				
-				this.sprite = PIXI.Sprite.fromImage(this.randomTrees[Math.floor(Math.random()*this.randomTrees.length)]);
-				this.sprite.anchor.set(0.5);
+				x = x + 100;
+			}
+			y = y + 100;
+		}
+	}
 
-				//Random scale from 0.1 to 0.2 and rotation from 0 to 2*pi
-    			this.sprite.scale.set(this.scales[Math.floor(Math.random()*this.scales.length)]);
-				this.sprite.rotation = Math.random()*2*Math.PI;
+	updateMinMax(point) {
+		if (point.x < this.xMin) this.xMin = point.x;
+		if (point.y < this.yMin) this.yMin = point.y;
+		if (point.x > this.xMax) this.xMax = point.x;
+		if (point.y > this.yMax) this.yMax = point.y;
+	}
 
-    			this.sprite.position.x = x_offset;
-    			this.sprite.position.y = y_offset;
-    			this.stage.addChild(this.sprite);
+	checkRange(obstacles, x, y) {
+		for (var i = 0; i < obstacles.length; i++) {
+			if (obstacles[i].type === 'route' && distanceToSegment({x:x, y:y}, obstacles[i].startpoint, obstacles[i].endpoint) < this.distanceToRoad) {
+				return true;
+			}
+			else if (obstacles[i].type === 'log' && distance({x:x, y:y}, {x:obstacles[i].x, y:obstacles[i].y}) < this.distanceToLog) {
+				return true;
+			}
+			else if (obstacles[i].type === 'logdeposit' && distance({x:x, y:y}, {x:obstacles[i].x, y:obstacles[i].y}) < this.distanceToLogDeposit) {
+				return true;
 			}
 		}
+		return false;
+	}
+
+	drawSprite(path, x, y) {
+		this.sprite = PIXI.Sprite.fromImage(path);
+		this.sprite.anchor.set(0.5);
+
+		//Random scale from 0.1 to 0.2 and rotation from 0 to 2*pi
+		this.sprite.scale.set(this.scales[Math.floor(Math.random()*this.scales.length)]);
+		this.sprite.rotation = Math.random()*2*Math.PI;
+
+		this.sprite.position.x = x;
+		this.sprite.position.y = y;
+		this.stage.addChild(this.sprite);
 	}
 }
