4bca73d94a0c6147b94a6e38fce0c5deec8709ef
Torbe10 <tonierikmartin@gmail.com>
2018-01-29 21:00:35 +0200

    Help page done with pictures (no licenses yet)

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,45 +1,172 @@
 import React, { Component } from 'react';
+import { LANG } from './lang';
 import './Help.css';
 
 export default class Help extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentCondImg: "static/gameplay.png",
+			condInfo: "normalDescription",
+			condValue: 1,
+			currentEditorImg: "static/editor_roadtool.png",
+			editorInfo: "roadtool",
+			editorValue: 1
+		};
+	}
+
+	handleEditorClick(e) {
+		var id = e.target.value;
+		var currentPic;
+		var currentInfo;
+		switch (id) {
+			case "1":
+				currentPic = "static/editor_roadtool.png";
+				currentInfo = "roadtool";
+				break;
+			case "2":
+				currentPic = "static/editor_logtool.png";
+				currentInfo = "logstool";
+				break;
+			case "3":
+				currentPic = "static/editor_deposittool.png";
+				currentInfo = "deposittool";
+				break;
+			case "4":
+				currentPic = "static/editor_trucktool.png";
+				currentInfo = "trucktool";
+				break;
+			case "5":
+				currentPic = "static/editor_removetool.png";
+				currentInfo = "removetool";
+				break;
+			default:
+				break;
+		}
+
+		this.setState({
+			currentEditorImg: currentPic,
+			editorInfo: currentInfo,
+			editorValue: id
+		});
+	}
+
+	handleConditionClick(e) {
+		var id = e.target.value;
+		var currentPic;
+		var currentInfo;
+		switch (id) {
+			case "1":
+				currentPic = "static/gameplay.png";
+				currentInfo = "normalDescription";
+				break;
+			case "2":
+				currentPic = "static/gp1.png";
+				currentInfo = "dyingDescription";
+				break;
+			case "3":
+				currentPic = "static/gp2.png";
+				currentInfo = "weightDescription";
+				break;
+			case "4":
+				currentPic = "static/gp3.png";
+				currentInfo = "onewayDescription";
+				break;
+			case "5":
+				currentPic = "static/gp4.png";
+				currentInfo = "fogDescription";
+				break;
+			default:
+				break;
+		}
+
+		this.setState({
+			currentCondImg: currentPic,
+			condInfo: currentInfo,
+			condValue: id
+		});
+	}
+
 	render() {
+
+		var tool = LANG[this.props.lang].help.editor[this.state.editorInfo];
+		var info = LANG[this.props.lang].help.gameplay[this.state.condInfo];
 		return (
 			<div className="Help">
 				<div className="help-content">
-					<div id="help-content-gameplay">
-						<span id="help-content-gameplay-title">Gameplay</span>
-						<p>
-							Goal of the game is to load all logs in the working area and
-							unload them into unloading stations. A game is completed when
-							all the logs are unloaded to their respective unloading stations.
-						</p>
-						<span className="h2">Controls</span><br/>
-						<p>
-							- Use arrow- or WASD- keys to move the truck.<br/>
-							- Pick up the logs using space or mouse left-click.<br/>
-							- Move the camera with mouse right-click or mouse middle-button.<br/>
-							- Choose a log or unloading station with buttons Q and E.<br/>
-							- Numpad keys 1-9 can be used to show/hide available logs.<br/>
+					<div className="h1">
+						<span id="help-content-gameplay-title">{LANG[this.props.lang].help.gameplay.gameplay}</span>						
+					</div>
+					<p>
+						{LANG[this.props.lang].help.gameplay.gameplayintro}
+					</p>
+					<p>
+						{LANG[this.props.lang].help.gameplay.gameplaydescription}
+					</p>
 
+					<div className="h2">
+						<span>{LANG[this.props.lang].help.gameplay.conditions}</span><br/>
+					</div>
+
+					<p>
+						{LANG[this.props.lang].help.gameplay.conditionsDescription}
+					</p>
+
+					<select name="menu-conditions" className="menu" onChange={this.handleConditionClick.bind(this)} value={this.state.condValue}>
+						<option value={1}>{LANG[this.props.lang].help.gameplay.normalMenu}</option>
+						<option value={2}>{LANG[this.props.lang].help.gameplay.limitedcrossingMenu}</option>
+						<option value={3}>{LANG[this.props.lang].help.gameplay.weightlimitMenu}</option>
+						<option value={4}>{LANG[this.props.lang].help.gameplay.onewayMenu}</option>
+						<option value={5}>{LANG[this.props.lang].help.gameplay.fogMenu}</option>
+					</select>
+
+					<div className="road-conditions">
+						<p>
+							{info}
+						</p>
+						<img src={this.state.currentCondImg}/>
+					</div>
+
+					<div className="h2">
+						<span>{LANG[this.props.lang].help.controls.controls}</span><br/>
+					</div>
+					<div id="controls">
+						<img src="static/controls.png"/>
+						<p>
+							{LANG[this.props.lang].help.controls.controls1}<br/>
+							{LANG[this.props.lang].help.controls.controls2}<br/>
+							{LANG[this.props.lang].help.controls.controls3}<br/>
+							{LANG[this.props.lang].help.controls.controls4}<br/>
+							{LANG[this.props.lang].help.controls.controls5}<br/>
+							{LANG[this.props.lang].help.controls.controls6}<br/>
+							{LANG[this.props.lang].help.controls.controls7}<br/>
 						</p>
 					</div>
-					<div id="help-content-editor">
-						<span id="help-content-editor-title">Editor</span>
-						<p>
-							In editor you can create new custom maps.
-						</p>
-						<span className="h2">Tools</span><br/>
-						<p>
-							- Roadtool<br/>
-							- Logstool<br/>
-							- Deposittool<br/>
-							- Trucktool<br/>
-						</p>
+					
+					<hr className="primary-hr"></hr>
+
+					<div className="h1">
+						<span id="help-content-editor-title">{LANG[this.props.lang].help.editor.editor}</span>
 					</div>
+
+					<select name="menu-editor" className="menu" onChange={this.handleEditorClick.bind(this)} value={this.state.editorValue}>
+						<option value={1}>{LANG[this.props.lang].help.editor.roadtoolMenu}</option>
+						<option value={2}>{LANG[this.props.lang].help.editor.logstoolMenu}</option>
+						<option value={3}>{LANG[this.props.lang].help.editor.deposittoolMenu}</option>
+						<option value={4}>{LANG[this.props.lang].help.editor.trucktoolMenu}</option>
+						<option value={5}>{LANG[this.props.lang].help.editor.removetoolMenu}</option>
+					</select>
+					<p>
+						{tool}
+					</p>
+					<img src={this.state.currentEditorImg}/>
+					<hr className="primary-hr"></hr>
 				</div>
 				<div className="help-license">
-					<span id="help-license-title">Licenses</span>
+					<div className="h1">
+						<span id="help-license-title">{LANG[this.props.lang].help.licenses.licenses}</span>
+					</div>
 				</div>
 			</div>
 		);
--- src/components/lang.js
+++ src/components/lang.js
@@ -180,6 +180,61 @@ export const LANG = {
       type4: "Birch pulp",
       type5: "Pine pulp",
       type6: "Spruce pulp",
+    },
+    help: {
+      gameplay: {
+        gameplay: "Gameplay",
+        gameplayintro: `Goal of the game is to transfer all the logs in the working area and unload them into unloading stations 
+                        in the most optimal way. The evaluation criteria depends on working time, driving distance, and overall cost. 
+                        A game is completed when all the logs are unloaded to their respective unloading stations, and a report is produced
+                        which includes statistics from the training session.`,
+        gameplaydescription: `A worksite can contain several types of logs. Each log weights 50kg regardless of the type. 
+                              A deposit will hold only 1 type of log. A deposit will be assigned a log type once a log is deposited to it.
+                              A deposit can be pre-assigned to contain only certain types of logs from the editor. Once logs have been deposited to a certain deposit,
+                              the logs cannot be removed from it. The truck's speed will be affected by the amount of logs loaded in the truck, and 
+                              reverse driving also affects the speed. Each log loading and unloading operation takes 15 seconds.`,
+        conditions: "Conditions",
+        normalMenu: "Normal",
+        limitedcrossingMenu: "Limited crossings",
+        weightlimitMenu: "Weight-limit",
+        onewayMenu: "One-way limit",
+        fogMenu: "Fog",
+        conditionsDescription: `There are a few road and weather conditions that the driver must take into account when working in a worksite.
+                                Check these conditions by selecting from a dropdown below.`,
+        normalDescription: `A worksite with normal conditions. Clear weather and roads have no limits.`,
+        dyingDescription: `This worksite contains a muddy road in the middle that can be crossed a maximum of 4 times.`,
+        weightDescription: `This worksite contains a weight-limited road which shows the maximum load you can have in addition to the truck. 
+                            One log weights 50kg.`,
+        onewayDescription: `This worksite contains a one-way road. The arrows represents the direction a truck can drive.`,
+        fogDescription: `This worksite contains is foggy which limits the visibility.`,
+      },
+      controls: {
+        controls: "Controls",
+        controls1: "1. Use Arrow keys or WASD-keys to control the truck.",
+        controls2: "2. Use Q and E buttons to select log for picking or unloading station for unloading.",
+        controls3: "3. Use keys 1-6 to show/hide logs on map.",
+        controls4: "4. Use Space to pick or unload a log.",
+        controls5: "5. Alternatively use Mouse left click to pick or unload a log.",
+        controls6: "6. Hold Mouse right button and drag to move camera.",
+        controls7: "7. Zoom in and out with Mouse middle button. Hold and drag to move camera."
+      },
+      editor: {
+        editor: "Editor",
+        roadtoolMenu: "Road tool",
+        logstoolMenu: "Log tool",
+        deposittoolMenu: "Deposit tool",
+        trucktoolMenu: "Truck tool",
+        removetoolMenu: "Remove tool",
+        roadtool: `With a Road tool you can make roads by clicking to a startpoint and to a desired endpoint.
+                   Double click to deselect the tool. Road conditions can also be set with a road tool.`,
+        logstool: `Use log tool to place logs in the working area.`,
+        deposittool: `Use deposit tool to place deposits in the working area.`,
+        trucktool: `Use truck tool to put a truck to your desired starting point and position. Swap the orientation with mouse right click.`,
+        removetool: `Use remove tool to remove objects from the working area.`,
+      },
+      licenses: {
+        licenses: "Licenses"
+      }
     }
   },
   fi: {
@@ -362,6 +417,59 @@ export const LANG = {
       type4: "Koivumassa",
       type5: "Mäntymassa",
       type6: "Kuusimassa",
+    },
+    help: {
+      gameplay: {
+        gameplay: "Pelaaminen",
+        gameplayintro: `Pelin tavoitteena on siirtää kaikki tukit työskentelyalueelta niiden keräyspisteille mahdollisimman kustannustehokkaasti.
+                        Pelin loputtua näytetään raportti, jonka arviointiperusteena käytetään työskentelyaikaa, kuljettua matkaa, sekä kustannuksia.
+                        Peli on ohi, kun kaikki tukit on kerätty niitä vastaaville keräyspisteille.`,
+        gameplaydescription: `Työskentelyalue voi pitää sisällään monta erityyppistä tukkia, joista jokainen painaa 50kg tyypistä riippumatta.
+                              Jokaiselle keräyspisteelle voidaan kerätä vain yhtä tukkityyppiä. Keräyspisteelle annetaan tukkityyppi sitten, kun 
+                              siihen puretaan ensimmäinen tukki. Tyyppi voidaan myös asettaa etukäteen editorissa. Kun keräyspisteelle on 
+                              purettu tukkeja, niitä ei voida enää kerätä pois siitä. Metsäkoneen nopeuteen vaikuttavat tukkien määrä koneessa, sekä
+                              peruuttaminen. Jokainen tukin lastausoperaatio vie 15 sekuntia.`,
+        conditions: "Olosuhteet",
+        normalMenu: "Normaali",
+        limitedcrossingMenu: "Ylitysrajoitus",
+        weightlimitMenu: "Painorajoitus",
+        onewayMenu: "Yksisuuntainen tie",
+        fogMenu: "Sumu",
+        conditionsDescription: `Työskentelyalueella voi olla myös erikoisteitä ja sääolosuhteita. Tarkista olosuhteet alla olevasta pudotusvalikosta.`,
+        normalDescription: `Normaali työskentelyalue. Selkeä sää ja ei erikoisteitä.`,
+        dyingDescription: `Tällä työskentelyalueella on mutainen tie, joka voidaan ylittää maksimissaan 4 kertaa.`,
+        weightDescription: `Tällä työskentelyalueella on painorajoitettu tie, joka näyttää maksimikuorman, minkä kanssa metsäkone voi ylittää sen.
+                            Yksi tukki painaa 50kg.`,
+        onewayDescription: `Tällä työskentelyalueella on yksisuuntainen tie. Nuolet osoittavat kulkusuunnan.`,
+        fogDescription: `Tällä työskentelyalueella on sumuinen sää, joka rajoittaa näkyvyyttä.`,
+      },
+      controls: {
+        controls: "Kontrollit",
+        controls1: "1. Käytä nuolinäppäimiä tai WASD-näppäimiä ohjataksesi metsäkonetta.",
+        controls2: "2. Käytä Q- ja E-näppäimiä valitaksesi tukin tai säilön.",
+        controls3: "3. Näppäimillä 1-6 voit piilottaa/näyttää tukkeja.",
+        controls4: "4. Nosta tukki tai laske tukki säilöön välilyönnillä.",
+        controls5: "5. Vaihtoehtoisesti käytä hiiren vasenta näppäintä tukin nostamiseen tai laskemiseen.",
+        controls6: "6. Pidä hiiren oikeaa näppäintä pohjassa ja vedä hiirtä liikuttaaksesi kameraa.",
+        controls7: "7. Zoomaa sisään tai ulos hiiren keskinäppäimellä. Pidä pohjassa ja vedä hiirtä liikuttaaksesi kameraa."
+      },
+      editor: {
+        editor: "Editori",
+        roadtoolMenu: "Tietyökalu",
+        logstoolMenu: "Tukkityökalu",
+        deposittoolMenu: "Säilötyökalu",
+        trucktoolMenu: "Metsäkonetyökalu",
+        removetoolMenu: "Poistotyökalu",
+        roadtool: `Tietyökalulla voidaan tehdä teitä klikkaamalla haluttuun aloituspisteeseen, sekä haluttuun päätepisteeseen.
+                   Tuplaklikkauksella poistetaan tietyökalun valinta. Erikoistiet voidaan myös asettaa tietyökalulla.`,
+        logstool: `Tukkityökalulla asetetaan tukkeja haluttuun paikkaan työskentelyalueella.`,
+        deposittool: `Säilötyökalulla asetetaan keräyspisteitä työskentelyalueelle.`,
+        trucktool: `Metsäkonetyökalulla laitetaan työkone haluttuun aloituspisteeseen. Hiiren oikealla painikkeella käännetään metsäkone.`,
+        removetool: `Poistotyökalulla voidaan poistaa objekteja työskentelyalueelta.`,
+      },
+      licenses: {
+        licenses: "Lisenssit"
+      }
     }
   }
 }
